f1ac3ced4512d331ce07cd8fd94ebf61f8665f88
Phap Dieu Duong <d2phap@gmail.com>
2017-12-07 14:24:56 +0800

    Added debug for VS Code

--- Source/webpack.config.js
+++ Source/webpack.config.js
@@ -2,31 +2,30 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-		entry: './renderer.js',
-		output: {
-			filename: './public/js/bundle.js'
-		},
-		target: "electron-main",
-		module: {
-			loaders: [
-				{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-				{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-				{
-					test: /\.scss$/,
-					loader: ExtractTextPlugin.extract('css-loader?url=false!sass-loader?sourceMap')
-				}
-			]
-		},
-		plugins: [
-				new ExtractTextPlugin('./public/css/app.css', {
-					allChunks: true
-				}),
-				new webpack.DefinePlugin({
-					'process.env': {
-						NODE_ENV: JSON.stringify('production')
-					}
-				}),
-				new webpack.optimize.UglifyJsPlugin()
+	entry: './renderer.js',
+	output: {
+		filename: './public/js/bundle.js'
+	},
+	target: "electron-main",
+	module: {
+		loaders: [
+			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+			{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+			{
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract('css-loader?url=false!sass-loader?sourceMap')
+			}
 		]
-	}
-	
+	},
+	plugins: [
+		new ExtractTextPlugin('./public/css/app.css', {
+			allChunks: true
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	]
+}
